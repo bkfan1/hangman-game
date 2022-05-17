9aa40dd42103c9407ada7151c11f832af9caf662
bkfan1 <blossomsc15@gmail.com>
2022-05-17 15:51:59 -0400

    Added more functions and states to handle the game's logic

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -4,31 +4,108 @@ import { words } from "../utils/words";
 export const GameContext = createContext();
 
 export const GameProvider = ({ children }) => {
-  const [matchWord, setMatchWord] = useState(null);
-  const [matchWordLetters, setMatchWordLetters] = useState(null);
+  const [matchWord, setMatchWord] = useState("");
+  const [matchWordLetters, setMatchWordLetters] = useState([]);
   const [guessedLetters, setGuessedLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
+
+  const [hangmanImage, setHangmanImage] = useState("");
+
+  const [remainingGuessingAttempts, setRemainingGuessingAttempts] = useState(5);
   const [clueCounter, setClueCounter] = useState(0);
   const [letterClue, setLetterClue] = useState("");
+
+  const [themeMode, setThemeMode] = useState("light");
   const [error, setError] = useState(null);
 
+  const setMatch = () => {
+    setRemainingGuessingAttempts(5);
+    setGuessedLetters([]);
+    setWrongLetters([]);
+    setError(null);
+
+    const randInt = Math.floor(Math.random() * words.length);
+
+    setMatchWordLetters(words[randInt].split(""));
+    setMatchWord(words[randInt]);
+    setClueCounter(3);
+  };
+
   useEffect(() => {
-      const setMatchWordAndLetters = ()=>{
-          const randInt = Math.floor(Math.random()* words.length);
-
-          setMatchWordLetters(words[randInt].split(""))
-          setClueCounter(words[randInt].length % 2 === 0 ? words[randInt].length/2 : Math.floor(words[randInt].length/3)+1)
-          setMatchWord(words[randInt])
-
-
-      }
-      setMatchWordAndLetters();
-   
+    setMatch();
   }, []);
+
+  useEffect(() => {
+    const handleHangmanImage = () => {
+      switch (remainingGuessingAttempts) {
+        case 5:
+          setHangmanImage(`/sprites/hangman/${themeMode}/state_0.png`);
+
+          break;
+        case 4:
+          setHangmanImage(`/sprites/hangman/${themeMode}/state_1.png`);
+          break;
+        case 3:
+          setHangmanImage(`/sprites/hangman/${themeMode}/state_2.png`);
+          break;
+        case 2:
+          setHangmanImage(`/sprites/hangman/${themeMode}/state_3.png`);
+          break;
+
+        case 1:
+          setHangmanImage(`/sprites/hangman/${themeMode}/state_4.png`);
+          break;
+
+        case 0:
+          setHangmanImage(`/sprites/hangman/${themeMode}/state_5.png`);
+          break;
+        default:
+          break;
+      }
+    };
+
+    handleHangmanImage();
+  }, [remainingGuessingAttempts, themeMode]);
+
+  const handleClickThemeMode = () => {
+    if (themeMode === "light") {
+      setThemeMode("dark");
+    } else {
+      setThemeMode("light");
+    }
+  };
 
   return (
     <>
-      <GameContext.Provider value={{matchWord, matchWordLetters, guessedLetters, setGuessedLetters, wrongLetters,setWrongLetters,letterClue,setLetterClue, clueCounter, setClueCounter, error, setError}}>{children}</GameContext.Provider>
+      <GameContext.Provider
+        value={{
+          setMatch,
+          hangmanImage,
+          themeMode,
+          handleClickThemeMode,
+
+          matchWord,
+          matchWordLetters,
+          guessedLetters,
+          wrongLetters,
+          setWrongLetters,
+
+          remainingGuessingAttempts,
+          setRemainingGuessingAttempts,
+          setGuessedLetters,
+
+          clueCounter,
+          setClueCounter,
+
+          letterClue,
+          setLetterClue,
+
+          error,
+          setError,
+        }}
+      >
+        {children}
+      </GameContext.Provider>
     </>
   );
 };
